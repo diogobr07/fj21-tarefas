18d0f4fb657455f9e074016ab4d9a939ac7e6d2b
diogo <diogobruno07@gmail.com>
2013-11-26 22:21:06 -0200

    add action and view file for list Tarefas

--- src/br/com/caelum/tarefas/controller/TarefasController.java
+++ src/br/com/caelum/tarefas/controller/TarefasController.java
@@ -1,8 +1,11 @@
 package br.com.caelum.tarefas.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -26,5 +29,13 @@ public class TarefasController {
 		JdbcTarefaDao dao = new JdbcTarefaDao();
 		dao.adiciona(tarefa);
 		return "tarefa/adicionada";
+	}
+	
+	@RequestMapping("listaTarefas")
+	public String lista(Model model){
+		JdbcTarefaDao dao = new JdbcTarefaDao();
+		List<Tarefa> tarefas = dao.lista();
+		model.addAttribute("tarefas", tarefas);
+		return "tarefa/lista";
 	}
 }
